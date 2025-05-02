682903d4be15905033dcd12c47a9513aed72a2a7
田村慶太 <tamurakeita@tamurakeitanoMacBook-Air.local>
2025-05-02 20:23:36 +0900

    Update3

--- netlify/functions/userinfo.js
+++ netlify/functions/userinfo.js
@@ -1,15 +1,35 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
-  const res = await fetch("http://192.168.11.18:3000/api/userinfo", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: event.body,
-  });
+  const method = event.httpMethod;
 
-  const body = await res.text();
+  if (method === "GET") {
+    const res = await fetch("http://192.168.11.18:3000/api/userinfo", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    });
+    const body = await res.text();
+    return {
+      statusCode: res.status,
+      body
+    };
+  }
+
+  if (method === "POST") {
+    const res = await fetch("http://192.168.11.18:3000/api/userinfo", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: event.body
+    });
+    const body = await res.text();
+    return {
+      statusCode: res.status,
+      body
+    };
+  }
+
   return {
-    statusCode: res.status,
-    body,
+    statusCode: 405,
+    body: "Method Not Allowed"
   };
 };
